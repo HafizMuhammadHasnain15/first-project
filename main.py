83af74888ee712f1575cf0ac323636a84d297550
HafizMuhammadHasnain15 <hafizmuhammadhasnain1512@gmail.com>
2026-05-15 16:25:06 +0500

    add and multiplication of any two numbers

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 name=input("enter your name : ")
 print(name.capitalize())
+
+num1=int(input("enter the first number : "))
+num2=int(input("enter the  second number : "))
+add=num1+num2
+print(add)
+multiplay=num1*num2
+print(multiplay)
