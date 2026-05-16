79ee62ae3d4b15f97227e6a7252d5dd6b038bc05
hafiz_hasnain_mughal <hafizmuhammadhasnain12@gmail.com>
2026-05-16 11:11:04 +0500

    making hte simple calculator

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 name=input("enter your name : ")
 print(name.capitalize())
 
-num1=int(input("enter the first number : "))
-num2=int(input("enter the  second number : "))
-add=num1+num2
-print(add)
-multiplay=num1*num2
-print(multiplay)
+num1=float(input("enter the first number : "))
+num2=float(input("enter the  second number : "))
+
+symbol=input("enter the symbol : ")
+if symbol =="+":
+    print(num1 + num2)
+elif symbol == "-":
+    print(num1 - num2)
+elif symbol == "*":
+    print(num1 * num2)
+elif symbol =="/":
+    if num2==0:
+        print("not possible ")
+    else:
+        print(num1/num2)
+else:
+    print("wronge input")
